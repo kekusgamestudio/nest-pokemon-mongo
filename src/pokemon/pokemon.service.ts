dd161fd85f82c7c06134a3bfe60455d4eeed4375
Sergio Martino <sergio.martino@modo.com.ar>
2023-06-12 23:08:06 -0300

    Added get all with pagination

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,6 +5,7 @@ import { Pokemon } from './entities/pokemon.entity';
 
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -29,8 +30,15 @@ export class PokemonService {
 
 
 
-  findAll() {
-    return `This action returns all pokemon`;
+  findAll(paginationDto: PaginationDto) {
+
+    const {limit = 10, page = 0} = paginationDto;
+
+    return this.pokemonModel.find()
+      .limit(limit)
+      .skip(limit * page)
+      .sort({nro: 1})
+      .select('-__v')
   }
 
 
